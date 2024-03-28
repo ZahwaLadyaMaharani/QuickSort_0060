bf128dd3d1d12054b08a9e165539eb723684bb62
ZahwaLadyaMaharani <zahwa.ladya.ft20@mail.umy.ac.id>
2024-03-28 11:55:14 +0800

    mencari elemen yang kurang dari atau sama dengan pivot di sebelah kanan partisi

--- QuickSortt/QuickSortt.cpp
+++ QuickSortt/QuickSortt.cpp
@@ -59,6 +59,13 @@ void q_short(int low, int high)
 		while ((arr[i] <= pivot) && (i <= high)) //step 5
 		{
 			i++; //step 6
+			cmp_count++;
+		}
+		cmp_count++;
+		//search for an element less than or equal to pivot
+		while ((arr[j] > pivot) && (j >= low)) //step 7
+		{
+
 		}
 	}
 }
