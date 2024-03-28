6997c63392121cb7abb78c313ceffe5873faa03d
ZahwaLadyaMaharani <zahwa.ladya.ft20@mail.umy.ac.id>
2024-03-28 11:33:17 +0800

    menukar elemen indeks

--- QuickSortt/QuickSortt.cpp
+++ QuickSortt/QuickSortt.cpp
@@ -32,4 +32,12 @@ void input() {
 		cin >> arr[i];
 	}
 }
+//swaps the lement at index x with the element at index y
+void swap(int x, int y)
+{
+	int temp = arr[x];
+	arr[x] = arr[y];
+	arr[y] = temp;
+	mov_count++;
+}
 
